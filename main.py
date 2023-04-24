65e909b64d8c136f7d2f81c16457f59fafe92eee
Fahim Ishrak <fahim.ishrak1@gmail.com>
2023-04-24 12:03:28 -0400

    added comments

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+# Import the necessary libraries and modules
 import os
 from pathlib import Path
 from docx import Document
-from word_handler import find_synonyms_in_paragraphs,read_paragraphs_from_docx
+from word_handler import find_synonyms_in_paragraphs, read_paragraphs_from_docx
 
+# Define a function named "main"
 def main():
+    # Prompt the user to enter a target directory and a search word
     target_directory = input("Enter the target directory: ")
     search_word = input("Enter the word you want to find synonyms for: ")
 
+    # Use os.walk to iterate over all files in the directory tree
     for root, _, files in os.walk(target_directory):
+        # Loop over all files in the current directory
         for file in files:
+            # Check if the file is a Word document
             if file.endswith(".docx"):
+                # Construct the full path to the file
                 file_path = Path(root) / file
+                # Read the paragraphs from the file
                 paragraphs = read_paragraphs_from_docx(file_path)
 
+                # Find synonyms of the search word in the paragraphs
                 synonyms = find_synonyms_in_paragraphs(paragraphs, search_word)
+                # If synonyms are found, print them to the console
                 if synonyms:
                     print(f"\nSynonyms found in file: {file_path.absolute()}")
                     for synonym in synonyms:
@@ -22,5 +32,6 @@ def main():
                               f"Line number: {synonym['line_number']}",
                               f"Similarity score: {synonym['similarity_score']}")
 
+# Check if the script is being run directly and call the "main" function if it is
 if __name__ == "__main__":
     main()
--- word_handler.py
+++ word_handler.py
@@ -1,38 +1,48 @@
+# Import the necessary libraries and modules
 import os
 import spacy
 from docx import Document
 import re
 
-
+# Define a function named "read_paragraphs_from_docx"
 def read_paragraphs_from_docx(docx_file_path):
+    # Load the Word document using the "docx" library
     doc = Document(docx_file_path)
     paragraphs = []
 
+    # Loop over all paragraphs in the document
     for paragraph in doc.paragraphs:
         # Replace newline characters with space
         cleaned_text = paragraph.text.replace('\n', ' ').strip()
+        # If the paragraph has text, add it to the list of paragraphs
         if cleaned_text:
             paragraphs.append(cleaned_text)
 
     return paragraphs
 
+# Load the medium model of the "spacy" library to access word vectors for similarity calculation
+nlp = spacy.load("en_core_web_md")
 
-nlp = spacy.load("en_core_web_md")  # Use the medium model to have access to word vectors for similarity calculation
-
-
-
-def find_synonyms_in_paragraphs(paragraphs, search_word,SIMILARITY_THRESHOLD=0.5):
+# Define a function named "find_synonyms_in_paragraphs"
+def find_synonyms_in_paragraphs(paragraphs, search_word, SIMILARITY_THRESHOLD=0.5):
+    # Process the search word using the "spacy" model
     search_tokens = nlp(search_word)
     synonyms = []
 
+    # Loop over all paragraphs and lines in the list of paragraphs
     for paragraph_number, paragraph in enumerate(paragraphs, start=1):
-        # Split paragraph into sentences using regex
+        # Split the paragraph into sentences using regex
         lines = re.split(r'[.!?] *', paragraph)
         for line_number, line in enumerate(lines, start=1):
+            # Process the line using the "spacy" model
             tokens = nlp(line)
+            # Loop over all tokens in the line
             for token in tokens:
+                # Check if the token is a word and is different from the search word
                 if token.is_alpha and token.lower_ != search_word.lower():
+                    # Calculate the similarity score between the search word and the token
                     similarity = search_tokens.similarity(token)
+                    # If the similarity score is above the threshold, add the token to the list of synonyms
                     if similarity > SIMILARITY_THRESHOLD:
                         synonyms.append({
                             'word': token.text,
@@ -42,4 +52,3 @@ def find_synonyms_in_paragraphs(paragraphs, search_word,SIMILARITY_THRESHOLD=0.5
                         })
 
     return synonyms
-
